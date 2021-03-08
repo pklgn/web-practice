0c0e8b39075bd5b1d5d53f9aaa2a16c09b1991e6
pklgn <petechka_petrov_201@mail.ru>
2021-03-09 02:28:53 +0300

    add two simple pages

--- src/Controller/MyFirstController.php
+++ src/Controller/MyFirstController.php
@@ -4,17 +4,20 @@
 namespace App\Controller;
 
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class MyFirstController
+class MyFirstController extends AbstractController
 {
     /**
      * @Route("/")
      */
     public function simpleanswer()
     {
-        return new Response('I would like to say hello to everybody!');
+        return $this->render('main/showmain.html.twig', [
+            'about' => ucwords('Ермаков Павел Константинович')
+        ]);
     }
 
     /**
@@ -22,6 +25,8 @@ class MyFirstController
      */
     public function smthnew()
     {
-        return new Response("That's a new page!");
+        return $this->render('about/show.html.twig', [
+            'about' => ucwords('Ермаков Павел Константинович')
+        ]);
     }
 }
