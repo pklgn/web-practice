40641ff1100e516781b6200a1ff1c9affebe1bb2
pklgn <petechka_petrov_201@mail.ru>
2021-04-04 19:04:44 +0300

    use php class

--- lw1/src/Controller/MyFirstController.php
+++ lw1/src/Controller/MyFirstController.php
@@ -5,22 +5,24 @@ namespace App\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use App\Modules\ImageList;
 
 class MyFirstController extends AbstractController
 {
     public function simple_answer()
     {
         return $this->render('main/main.html.twig', [
-            'about' => ucwords('Ермаков Павел Константинович')
+            'about' => ucwords('Ермаков Павел Константинович'),
         ]);
     }
 
     public function about()
     {
+        $images = new ImageList('buran');
+        $image = $images->chooseOne();
         return $this->render('about/about.html.twig', [
-            'about' => ucwords('Ермаков Павел Константинович')
+            'about' => ucwords('Ермаков Павел Константинович'),
+            'image' => $image,
         ]);
     }
 }
