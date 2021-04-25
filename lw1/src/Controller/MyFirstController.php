2780b2a65bbbb1ea2b7473169454e5aa89c26dfb
pklgn <petechka_petrov_201@mail.ru>
2021-04-25 14:04:28 +0300

    change hobbies titles

--- lw1/src/Controller/MyFirstController.php
+++ lw1/src/Controller/MyFirstController.php
@@ -6,27 +6,30 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Modules\ImageProvider;
+use Symfony\Component\HttpFoundation\Response;
 
 class MyFirstController extends AbstractController
 {
-    public function simple_answer()
+    public function simple_answer(): Response
     {
         return $this->render('main/main.html.twig', [
             'about' => ucwords('Ермаков Павел Константинович'),
         ]);
     }
 
-    public function about()
+    public function about(): Response
     {
-        $galleryTitles = ['buran', 'ISS', 'space'];
+        $galleryTitles = ['Buran', 'ISS', 'Space'];
         foreach ($galleryTitles as $galleryTitle)
             $galleryHobbies[$galleryTitle] = new ImageProvider($galleryTitle);
         foreach ($galleryHobbies as $title => $galleryHobby)
             $imagesLists[$title] = $galleryHobby->gatherChosenImages();
-        return $this->render('about/about.html.twig', [
-            'about' => ucwords('Ермаков Павел Константинович'),
-            'titles' => $galleryTitles,
-            'imagesLists' => $imagesLists,
-        ]);
+        if (isset($imagesLists)) {
+            return $this->render('about/about.html.twig', [
+                'about' => ucwords('Ермаков Павел Константинович'),
+                'titles' => $galleryTitles,
+                'imagesLists' => $imagesLists,
+            ]);
+        }
     }
 }
