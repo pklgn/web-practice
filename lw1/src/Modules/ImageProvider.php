fd75eb868b35c0d36e6b364e57a715a697ee3519
pklgn <petechka_petrov_201@mail.ru>
2021-04-25 14:07:46 +0300

    add ImageProvider

---
+++ lw1/src/Modules/ImageProvider.php
@@ -0,0 +1,42 @@
+<?php
+namespace App\Modules;
+use IvanUskov\ImageSpider\ImageSpider;
+
+class ImageProvider
+{
+    public string $hobby;
+
+    public function __construct(string $keyWord)
+    {
+        $this->hobby = $keyWord;
+    }
+
+    public function getImagesArray(): array
+    {
+        $array = ImageSpider::find($this->hobby);
+        return $array;
+    }
+
+    public function chooseFiveIndexes(): array
+    {
+        $imagesNumber = count($this->getImagesArray());
+        $minIndexVal = 0;
+        $maxIndexVal = $imagesNumber - 1;
+        for($i = 0; $i < 5; $i++)
+        {
+            $chosenIndexes[] = random_int($minIndexVal, $maxIndexVal);
+        }
+        return $chosenIndexes;
+    }
+
+    public function gatherChosenImages(): array
+    {
+        $initGallery = $this->getImagesArray();
+        $indexes = $this->chooseFiveIndexes();
+        foreach ($indexes as $index)
+        {
+            $chosenGallery[] = $initGallery[$index];
+        }
+        return $chosenGallery;
+    }
+}
